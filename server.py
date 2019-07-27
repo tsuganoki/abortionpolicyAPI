97f6afc7092a21ce4a89642897b27d2449b98df7
Tilia <lindsey.snider@gmail.com>
2019-07-26 17:52:05 -0700

    moved test api call into a a local scope/function. began to split topic data apart on results page.

--- server.py
+++ server.py
@@ -12,18 +12,19 @@ import requests
 
 apikey = open('token.txt').read()
 
+def test():
+    url = 'https://api.abortionpolicyapi.com/v1/gestational_limits/states'
+    headers = { 'token': apikey }
+    r = requests.get(url, headers=headers)
 
-url = 'https://api.abortionpolicyapi.com/v1/gestational_limits/states'
-headers = { 'token': apikey }
-r = requests.get(url, headers=headers)
+    states = r.json()
 
-states = r.json()
-
-limit_at_viability = set(state for state in states.keys() if states[state].get('banned_after_weeks_since_LMP') == 99)
+    limit_at_viability = set(state for state in states.keys() if states[state].get('banned_after_weeks_since_LMP') == 99)
 
 
-message = f"The states that ban abortion at viability are: {', '.join(limit_at_viability)}"
-print(message)
+    message = f"The states that ban abortion at viability are: {', '.join(limit_at_viability)}"
+    print(message)
+# test()
 
 app = Flask(__name__)
 
